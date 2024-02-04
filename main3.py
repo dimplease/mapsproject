6e6ddb089b3ea5a5d7740db30e3789020400ead6
maria5237 <152502129+maria5237@users.noreply.github.com>
2024-02-04 23:51:17 +0700

    Add files via upload

--- main3.py
+++ main3.py
@@ -1,15 +1,18 @@
 import os
 import sys
-
 import requests
 from PyQt5.QtGui import QPixmap
 from PyQt5.QtWidgets import QApplication, QWidget, QLabel
 
 SCREEN_SIZE = [600, 450]
 
+
 class Example(QWidget):
     def __init__(self):
         self.spn = 0.002
+        self.ll1 = 37.587945442466825
+        self.ll2 = 55.73402552478429
+
         self.form = 'map'
         self.counterform = 0
         super().__init__()
@@ -23,8 +26,9 @@ class Example(QWidget):
             elif self.spn >= 90:
                 self.spn = 0.002
             else:
-                self.spn += 0.03
+                self.spn += 0.003
         elif str(event.key()) == '16777237':
+            self.spn -= 0.001
             if self.spn <= 0:
                 self.spn = 0.002
             elif self.spn < 0.08 and self.spn > 0:
@@ -41,13 +45,36 @@ class Example(QWidget):
             elif self.counterform == 2:
                 self.form = 'sat,skl'
             self.counterform += 1
+        elif event.key() == 68:
+            if self.ll1 < 180:
+                self.ll1 += 1
+            if self.ll1 >= 180:
+                self.ll1 = 37.587945442466825
+        elif event.key() == 65:
+            if self.ll1 > -180:
+                self.ll1 -= 1
+            if self.ll1 <= -180:
+                self.ll1 = 37.587945442466825
+        elif event.key() == 87:
+            if self.ll2 < 85:
+                self.ll2 += 10
+            if self.ll2 >= 85:
+                self.ll2 = 55.73402552478429
+        elif event.key() == 83:
+            if self.ll2 > -85:
+                self.ll2 -= 10
+            if self.ll2 <= -85:
+                self.ll2 = 55.73402552478429
 
         self.image.clear()
         self.getImage()
+        print(event.key())
+        print(self.ll2)
 
 
     def getImage(self):
-        map_request = f"http://static-maps.yandex.ru/1.x/?ll=37.587945442466825,55.73402552478429&spn={self.spn},{self.spn}&l={self.form}"
+        map_request = f"http://static-maps.yandex.ru/1.x/?ll={self.ll1},{self.ll2}&spn={self.spn},{self.spn}&l=map"
+        map_request = f"http://static-maps.yandex.ru/1.x/?ll={self.ll1},{self.ll2}&spn={self.spn},{self.spn}&l={self.form}"
         response = requests.get(map_request)
 
         if not response:
@@ -55,7 +82,6 @@ class Example(QWidget):
             print(map_request)
             print("Http статус:", response.status_code, "(", response.reason, ")")
             sys.exit(1)
-
         # Запишем полученное изображение в файл.
         self.map_file = "map.png"
         with open(self.map_file, "wb") as file:
@@ -65,10 +91,8 @@ class Example(QWidget):
     def initUI(self):
         self.setGeometry(100, 100, *SCREEN_SIZE)
         self.setWindowTitle('Отображение карты')
-
         ## Изображение
         self.pixmap = QPixmap(self.map_file)
-
         self.image.move(0, 0)
         self.image.resize(600, 450)
         self.image.setPixmap(self.pixmap)
@@ -77,12 +101,8 @@ class Example(QWidget):
         """При закрытии формы подчищаем за собой"""
         os.remove(self.map_file)
 
-
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     ex = Example()
     ex.show()
     sys.exit(app.exec())
-
-
-
